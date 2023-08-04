37c8eeb0334ae5c3d1ee2fb4b4bea469f592f83a
Lukasz Jasionowski <jasionowski.lukasz@gmail.com>
2023-08-04 22:20:59 +0200

    Add and use connectDB & Books

--- server/index.js
+++ server/index.js
@@ -1,13 +1,25 @@
 require("dotenv").config();
 const cors = require("cors");
 const express = require("express");
+const connectDB = require("./connectDB");
+const Book = require("./models/Books");
 
 const app = express();
 const PORT = process.env.PORT || 8000;
 
+connectDB();
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.get("/api/books", async (req, res) => {
+    try {
+        const data = await Book.find({});
+        res.json(data);
+    } catch (error) {
+        res.status(500).json({ error: "An error occured while fetching books." });
+    }
+});
 
 app.get("/", (req, res) => {
     res.json("Hello!");
