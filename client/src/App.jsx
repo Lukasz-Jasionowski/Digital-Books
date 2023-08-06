fa6e5d4957d641179129f78f6260962fde3e4b4a
Lukasz Jasionowski <jasionowski.lukasz@gmail.com>
2023-08-06 22:37:44 +0200

    Import and use components and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,21 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./routes/home/Home";
+import About from "./routes/about/About";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
 function App() {
 
   return (
     <>
-
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+        <Footer />
+      </Router>
     </>
   )
 }
