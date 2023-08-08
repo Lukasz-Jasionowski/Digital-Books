0581d1774160e926d586e05fcdc8b9c9611b4a88
Lukasz Jasionowski <jasionowski.lukasz@gmail.com>
2023-08-08 14:24:51 +0200

    Add Route to create new book

--- server/index.js
+++ server/index.js
@@ -39,6 +39,23 @@ app.get("/api/books/:slug", async (req, res) => {
     }
 });
 
+app.post("/api/books", async (req, res) => {
+    try {
+        const newBook = new Book({
+            title: req.body.title,
+            slug: req.body.slug,
+            stars: req.body.stars,
+            description: req.body.description,
+            category: req.body.category,
+            // thumbnail: req.file.thumbnail TO DO
+        })
+        await Book.create(newBook);
+        res.json("Data Submitted");
+    } catch (error) {
+        res.status(500).json({ error: "An error occured while adding book." });
+    }
+});
+
 app.get("/", (req, res) => {
     res.json("Hello!");
 });
